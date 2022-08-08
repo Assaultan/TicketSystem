1f89b9dda32803df95d297a50add6fefa8d58e49
unknown <dauleshg06@gmail.com>
2022-08-08 12:26:13 +0530

    project setup

---
+++ ticket_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+# from django.contrib.auth.models import User
+
+# Create your models here.
+class Ticket(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=256,null=True)
+    
+    STATUS_CHOICES = [
+        ('open','open'),
+        ('close','close'),
+    ]
+    status = models.CharField(max_length=5,
+            choices=STATUS_CHOICES,
+            default="OPEN")
+    PRIORITY_CHOICES = [
+        ('low','low'),
+        ('medium','medium'),
+        ('high','high'),
+    ]
+    priority = models.CharField(max_length=6,
+            choices=PRIORITY_CHOICES,
+            default="LOW")
+    # assignedTo = models.ForeignKey(User,on_delete=models.CASCADE)
+    createdAt = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
